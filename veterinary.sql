6e1b2dba2aea8ab431f00e674a13decb2de72a8e
Nathyieee <itsyoboinate11@gmail.com>
2023-11-08 11:47:23 +0800

    feat/insert-10-rows-appointments

--- veterinary.sql
+++ veterinary.sql
@@ -5,7 +5,8 @@ CREATE TABLE owners(
 	ofirstname VARCHAR(50) NOT NULL,
 	olastname VARCHAR(50) NOT NULL,
 	address VARCHAR(15) NOT NULL,
-	email VARCHAR(100) NOT NULL
+	email VARCHAR(100) NOT NULL,
+    phone VARCHAR(15) NOT NULL
 );
 
 CREATE TABLE animals(
@@ -63,12 +64,13 @@ VALUES
     (2, 'Maria', 'Santos', '456 Eim St, Quezon City', '987-654-3210', 'maria@example.com'),
     (3, 'Antonio', 'Gonzales', '789 Oak St, Cebu', '555-123-45678', 'antonio@example.com'),
     (4, 'Elena', 'Rodriguez', '345 Pine St, Davao', '777-888-9999', 'elena@example.com'),
-    (5, 'Ricardo', 'Lim', '987 Cedar St, Makati', '222-333-4444', ricardo@example.com'),
-    (6, 'Isabela', 'Reyes', '111 Acacia St, Pasig', '999-000-1111', isabel@example.com'),
+    (5, 'Ricardo', 'Lim', '987 Cedar St, Makati', '222-333-4444', 'ricardo@example.com'),
+    (6, 'Isabela', 'Reyes', '111 Acacia St, Pasig', '999-000-1111', 'isabel@example.com'),
     (7, 'Luis', 'Torres', '555 Maple St, Mandaluyong', '123-555-7777', 'luis@example.com'),
     (8, 'Carmen', 'Fernandez', '222 Birch St, Taguig', '333-222-1111', 'carmen@example.com'),
-    (9, 'Pedro', 'Santillian, '888 Spruce St, Bacolod', '888-777-3333', 'pedro@example.com'),
+    (9, 'Pedro', 'Santillian', '888 Spruce St, Bacolod', '888-777-3333', 'pedro@example.com'),
     (10, 'Sofia', 'Villianueva', '777 Walnut St, Iloilo', '111-999-3333', 'sofia@example.com');
+
 
     INSERT INTO animals(animalid, name, species, breed, dateofbirth, gender, color, ownerid)
 VALUES
@@ -82,3 +84,16 @@ VALUES
 (8, 'Simba', 'Cat', 'Bengal', 2018-06-18, 'Male', 'Spotted Rosetted', 8),
 (9, 'Max', 'Dog', 'Dachshund', 2020-07-14, 'Male', 'Black and Tan', 9),
 (10,'Cleo', 'Cat', 'Ragdoll', 2019-12-22, 'Female', 'Seal Point', 10);
+
+INSERT INTO appointments (appointid, animalid, appointdate, reason)
+VALUES
+(1, 1, '2023-01-05', 'Annual check-up'),
+(2, 2, '2023-01-10', 'Vaccination'),
+(3, 3, '2023-02-02', 'Injury assessment'),
+(4, 4, '2023-02-15', 'Dental cleaning'),
+(5, 5, '2023-03-05', 'Skin condition'),
+(6, 6, '2023-03-10', 'Check for fleas'),
+(7, 7, '2023-04-12', 'Vaccination'),
+(8, 8, '2023-04-18', 'Spaying/neutering'),
+(9, 9, '2023-05-02', 'Allergy treatment'),
+(10,10, '2023-05-20', 'Eye infection');
